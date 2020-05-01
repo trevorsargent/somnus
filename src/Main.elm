7a9cba19620d6125e5cbb7b53fdc3db2c4b6d323
Trevor Sargent <trevor@lucid.rocks>
2020-04-30 17:57:16 -0700

    wip: sync

--- src/Main.elm
+++ src/Main.elm
@@ -2,39 +2,61 @@ module Main exposing (main)
 
 import Browser
 
-import Html exposing (Html, div, input)
+import Html exposing (Html, div, input, p, text)
 import Html.Events exposing (onInput)
 import Html.Attributes exposing (placeholder, value)
+import List exposing (map)
+import Debug exposing (log)
+import Keyboard exposing (Key(..))
+import Keyboard.Events as Keyboard
 
 type alias Model = 
-  { value: Float
+  { readout: List String
   , text: String
   }
 type Msg 
-  = Change String
+  = Input String
+  | Submit 
+  | NoOp
 
 init: Model
 init = 
-  { value = 0.0
-  , text = "Hello, World"
+  { text = "" 
+  , readout = []
   }
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of 
-    Change newValue -> 
-      {model | text = String.reverse newValue}
+    Input newValue -> 
+      {model | text = log "new" newValue}
+    Submit  -> 
+      { model 
+      | text = ""
+      , readout = log "readout " model.text :: model.readout
+      }
+    NoOp -> 
+      model
+
+subscriptions : Model -> Sub Msg  
+subscriptions model =
+    Sub.map KeyboardMsg Keyboard.subscriptions
 
 view: Model -> Html Msg 
 view model =
   div []
-    [ 
-      console model.text "Start Here"
+    [ readout model.readout   
+    , console model.text "Start Here"
     ]
+
+
+readout: (List String) -> Html Msg
+readout list = 
+  div [] (map (\l -> p [] [text l]) list)
 
 console : String -> String ->  Html Msg
 console v p  =
-  input[ value v, placeholder p, onInput Change ][]
+  input[ value v, placeholder p, onInput Input ][]
 
 main: Program () Model Msg
 main = Browser.sandbox { init = init, update = update, view = view }
